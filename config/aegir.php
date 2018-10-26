78e27103b5c69b9fd5fb303313e46f3b8f168ebb
André Luiz Girol <andregirol@gmail.com>
2018-10-26 17:17:38 -0300

    Criado Arquivo de Configuração para o Aegir

---
+++ config/aegir.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+    |--------------------------------------------------------------------------
+    | Configurações do Aegir
+    |--------------------------------------------------------------------------
+    | 
+    | TODO: Talvez documentar melhor esse arquivo
+    |
+    */
+return [
+    'key' => env('AEGIR_KEY', false),
+    'protocol' => env('AEGIR_PROTOCOL', 'http'),
+    'host' => env('AEGIR_HOST', false)
+];
